d37328e132f288a8274b4c4db4fadb11c4816655
excelpatrick917 <excelpatrick917@gmail.com>
2026-01-14 12:35:47 +0000

    Fix public seller controller imports

--- lib/features/sellers/presentation/controllers/public_seller_controller.dart
+++ lib/features/sellers/presentation/controllers/public_seller_controller.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../product/presentation/controllers/product_details_controller.dart';
-import '../data/public_seller_repository_impl.dart';
-import '../domain/public_seller_profile.dart';
+import '../../data/public_seller_repository_impl.dart';
+import '../../domain/public_seller_profile.dart';
+import '../../../product/presentation/controllers/product_details_controller.dart';
 
 final publicSellerProfileProvider = FutureProvider.family<PublicSellerProfile, int>((ref, sellerId) async {
   return ref.watch(publicSellerRepositoryProvider).getSeller(sellerId);
